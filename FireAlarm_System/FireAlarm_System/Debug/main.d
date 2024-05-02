fb97a1cf8a0af8a69274dd5acaec8de7a617940a
HESHAM47GAMAL <hesham.shehatah00@eng-st.cu.edu.eg>
2024-05-02 14:46:56 +0300

    add WS & simulation

---
+++ FireAlarm_System/FireAlarm_System/Debug/main.d
@@ -0,0 +1,105 @@
+main.d main.o: .././main.c .././MCAL/USART/USART_interface.h \
+ .././MCAL/USART/../../SERVICE/STD_TYPES.h .././MCAL/USART/USART_config.h \
+ .././MCAL/ADC/ADC_interface.h .././MCAL/ADC/../../SERVICE/STD_TYPES.h \
+ .././MCAL/ADC/../GPIO/GPIO_interface.h \
+ .././MCAL/ADC/../GPIO/../../SERVICE/STD_TYPES.h \
+ .././MCAL/ADC/ADC_config.h .././HAL/LCD/LCD_interface.h \
+ .././HAL/LCD/../../SERVICE/STD_TYPES.h \
+ .././HAL/NTC_Temperature/NTC_interface.h \
+ .././HAL/NTC_Temperature/../../SERVICE/STD_TYPES.h \
+ ../FreeRTOS/Inc/FreeRTOS.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../FreeRTOS/Inc/FreeRTOSConfig.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom32.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ ../FreeRTOS/Inc/projdefs.h ../FreeRTOS/Inc/portable.h \
+ ../FreeRTOS/Inc/portmacro.h ../FreeRTOS/Inc/mpu_wrappers.h \
+ ../FreeRTOS/Inc/task.h ../FreeRTOS/Inc/list.h ../FreeRTOS/Inc/queue.h \
+ ../FreeRTOS/Inc/semphr.h ../FreeRTOS/Inc/queue.h \
+ ../FreeRTOS/Inc/event_groups.h ../FreeRTOS/Inc/timers.h \
+ ../FreeRTOS/Inc/task.h
+
+.././MCAL/USART/USART_interface.h:
+
+.././MCAL/USART/../../SERVICE/STD_TYPES.h:
+
+.././MCAL/USART/USART_config.h:
+
+.././MCAL/ADC/ADC_interface.h:
+
+.././MCAL/ADC/../../SERVICE/STD_TYPES.h:
+
+.././MCAL/ADC/../GPIO/GPIO_interface.h:
+
+.././MCAL/ADC/../GPIO/../../SERVICE/STD_TYPES.h:
+
+.././MCAL/ADC/ADC_config.h:
+
+.././HAL/LCD/LCD_interface.h:
+
+.././HAL/LCD/../../SERVICE/STD_TYPES.h:
+
+.././HAL/NTC_Temperature/NTC_interface.h:
+
+.././HAL/NTC_Temperature/../../SERVICE/STD_TYPES.h:
+
+../FreeRTOS/Inc/FreeRTOS.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../FreeRTOS/Inc/FreeRTOSConfig.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom32.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+../FreeRTOS/Inc/projdefs.h:
+
+../FreeRTOS/Inc/portable.h:
+
+../FreeRTOS/Inc/portmacro.h:
+
+../FreeRTOS/Inc/mpu_wrappers.h:
+
+../FreeRTOS/Inc/task.h:
+
+../FreeRTOS/Inc/list.h:
+
+../FreeRTOS/Inc/queue.h:
+
+../FreeRTOS/Inc/semphr.h:
+
+../FreeRTOS/Inc/queue.h:
+
+../FreeRTOS/Inc/event_groups.h:
+
+../FreeRTOS/Inc/timers.h:
+
+../FreeRTOS/Inc/task.h:
---
+++ FireAlarm_System/FireAlarm_System/main.c
@@ -0,0 +1,358 @@
+/*
+ * FireAlarm_System.cpp
+ *
+ * Created: 4/25/2024 6:22:08 PM
+ * Author : moham
+ */ 
+
+#include "MCAL/USART/USART_interface.h"
+#include "MCAL/ADC/ADC_interface.h"
+
+#include "HAL/LCD/LCD_interface.h"
+#include "HAL/NTC_Temperature/NTC_interface.h"
+
+
+#define ExceedTemperature    (1 << 0)
+#define SystemFireState		 (1 << 1)
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+#include "event_groups.h"
+// responsible for Initialize GPIO , ADC , UART , LCD and NTC temperature
+void System_Init(void);
+
+void LCD_MAINInit(void);
+
+// 
+// void T_T1(void* pvParam);
+// void T_T2(void* pvParam);
+void T_T3_Catch_Update_Temp(void * pvparam);
+void T_T4_UART_listen(void * pvparam);
+void T_T5_Update_LCD(void *pvparam);
+void T_T6_SwitchBetHaz_Nor(void *pvparam);
+
+void F_fireStateScreen(void);
+
+
+
+unsigned char Current_Temp  ;
+unsigned char thresold_Temp = 80;
+boolean AlarmState = TRUE; 
+
+
+SemaphoreHandle_t xMutexLCD;
+// SemaphoreHandle_t XFiresystemState_Semph ;
+
+QueueHandle_t	MessQUart_Thres_temp = NULL;
+QueueHandle_t 	MessMailuart_fireState = NULL;
+
+EventGroupHandle_t 	egEvents = NULL;
+EventBits_t			ebValues = 0;
+
+int main(void)
+{
+
+	System_Init();
+	LCD_MAINInit();
+	
+	xMutexLCD = xSemaphoreCreateMutex();
+	// XFiresystemState_Semph = xSemaphoreCreateBinary();
+	MessQUart_Thres_temp = xQueueCreate(3,3 * sizeof(uint8) );	
+	MessMailuart_fireState = xQueueCreate(3, sizeof(uint8));
+
+	egEvents = xEventGroupCreate();
+
+
+
+	xTaskCreate(T_T6_SwitchBetHaz_Nor, "Emergency_normal", 200, NULL, 4, NULL);
+	xTaskCreate(T_T3_Catch_Update_Temp, "Update Current Temp", 100, &Current_Temp, 3, NULL);
+	xTaskCreate(T_T5_Update_LCD, "update Threshold_state", 100, NULL, 2, NULL);
+	xTaskCreate(T_T4_UART_listen, "Recieve UART Mess", 100, NULL, 1, NULL);
+
+	xEventGroupSetBits(egEvents,SystemFireState); // As I by defualt make system Fire Enable
+
+	vTaskStartScheduler();
+
+	return 0 ;
+}
+
+
+
+void T_T3_Catch_Update_Temp(void * pvparam)
+{
+	
+	while(1)
+	{
+		/*	Read Current temperature sensor value*/
+		Current_Temp = NTC_GetTemperature();
+		
+		/*	Check if current temperature >= Threshold temperature*/
+		if(Current_Temp >= thresold_Temp )
+		{
+			/*	set event of exceeding temperature	*/
+			xEventGroupSetBits(egEvents,ExceedTemperature);
+		}
+		else
+		{
+			/*	Clear event of exceeding temperature	*/
+			xEventGroupClearBits(egEvents,ExceedTemperature);
+
+		}
+
+		/*	take mutex to access LCD shared resource*/
+		if( xSemaphoreTake( xMutexLCD, ( TickType_t ) 100 ) == pdTRUE )
+		{
+			/*	Dispalay value of current Temperature in LCD */
+			LCD_MoveCursor(0,10);
+			LCD_intToString(Current_Temp);
+			if(Current_Temp < 100)
+			{
+				LCD_DisplayString((uint8 *)" ");
+			}
+			// USART_SendStringPolling((uint8 *)"ADC update lcd\r");
+			xSemaphoreGive( xMutexLCD );
+		}
+		/*	Failed to take mutex*/
+		else
+		{
+			/*	tell that failled to update LCD	*/
+			USART_SendStringPolling((uint8 *)"ADC Failed update lcd\r");
+		}
+
+		vTaskDelay(500);/*	As I want to take new Temperature Value every half second	*/
+	}
+}
+
+
+void T_T4_UART_listen(void * pvparam)
+{
+	
+	uint8 u8Ind = 0;
+	uint8 txMsg_UART[4] ={};
+	uint8 u8Data = 0;
+	boolean update_thresould_temp = FALSE ;
+	while(1){
+		/*	Check if received any new data */
+		if(UART_receiveByteUnblocking(&u8Data)){
+			
+			
+			
+			if(u8Data == 'G') /*	Will update flag of Threshold Temperature */
+			{
+				update_thresould_temp = TRUE ;
+			}
+			else if( (update_thresould_temp == TRUE) && ((u8Data >= '0') && (u8Data <= '9')) )
+			{
+				txMsg_UART[u8Ind] = u8Data;
+				u8Ind++;
+			}
+			else if( (update_thresould_temp == TRUE) && (u8Data == 'K')) /*	Accept new threshold temperature value*/
+			{
+				txMsg_UART[u8Ind] = 0;
+				xQueueSend(MessQUart_Thres_temp, txMsg_UART, portMAX_DELAY);
+				update_thresould_temp = FALSE ;
+				u8Ind = 0;	
+			}
+			else if( (update_thresould_temp == TRUE) && (u8Data == 'C')) /*	Cancel new threshold temperature value*/
+			{
+				update_thresould_temp = FALSE ;
+				u8Ind = 0;
+			}
+			else if(u8Data == 'T') /*	Will toggle state of Alarm system*/
+			{
+				xQueueSend(MessMailuart_fireState, &u8Data, portMAX_DELAY);
+			}
+			else if(u8Data == 'D') /*	Will Disbale Alarm system*/
+			{
+				xQueueSend(MessMailuart_fireState, &u8Data, portMAX_DELAY);
+			}
+		}
+	}
+}
+
+
+void T_T5_Update_LCD(void *pvparam)
+{
+	uint8 rxMsg_thresholdTemp[4] = {};
+	uint8 rxmsg_fireState ;
+	while(1)
+	{
+		/*	handle received thresould temperature value and display it */
+		if(xQueueReceive(MessQUart_Thres_temp, rxMsg_thresholdTemp, 100))
+		{
+			thresold_Temp = rxMsg_thresholdTemp[0] - '0';
+			thresold_Temp *= 10 ;
+			thresold_Temp += rxMsg_thresholdTemp[1] - '0';
+
+			/*	Update Event state if thresold_Temp <= Current_Temp */
+			if(thresold_Temp <= Current_Temp)
+			{
+				/*	set event of exceeding temperature	*/
+				xEventGroupSetBits(egEvents,ExceedTemperature);
+				
+
+			}
+			/*	No need to update threshold value	*/
+			else
+			{
+				/*	Clear event of exceeding temperature	*/
+				xEventGroupClearBits(egEvents,ExceedTemperature);
+			}
+
+			if( xSemaphoreTake( xMutexLCD, ( TickType_t ) 100 ) == pdTRUE )
+			{
+				LCD_DisplayStringRowCol(rxMsg_thresholdTemp,1,16);
+				USART_SendStringPolling((uint8 *)"Thre update lcd\r");
+				xSemaphoreGive( xMutexLCD );
+			}
+			else
+			{
+				USART_SendStringPolling((uint8 *)"thre Failed update lcd\r");
+			}
+		}
+		else
+		{
+
+		}
+
+		/*	handle state of firing system if it enabled or disabled and update lcd with this change	*/
+		if(xQueueReceive(MessMailuart_fireState, &rxmsg_fireState, 100))
+		{
+			/*	Disable state of fire system */
+			if(rxmsg_fireState == 'D')
+			{
+				AlarmState = FALSE ;
+				xEventGroupClearBits(egEvents,SystemFireState);
+				if( xSemaphoreTake( xMutexLCD, ( TickType_t ) 400 ) == pdTRUE )
+				{
+					LCD_DisplayStringRowCol((uint8 *)"D",2,13);	
+					xSemaphoreGive( xMutexLCD );
+				}
+				else 
+				{
+
+				}
+			}
+			/*	Toggle state of fire system */
+			else if(rxmsg_fireState == 'T')
+			{
+				
+				if(AlarmState == TRUE)
+				{
+					AlarmState = FALSE ;
+					xEventGroupClearBits(egEvents,SystemFireState);
+					if( xSemaphoreTake( xMutexLCD, ( TickType_t ) 400 ) == pdTRUE )
+					{
+						LCD_DisplayStringRowCol((uint8 *)"D",2,13);	
+						xSemaphoreGive( xMutexLCD );
+					}
+					else 
+					{
+
+					}
+				}
+				else if(AlarmState == FALSE)
+				{
+					AlarmState = TRUE ;
+					xEventGroupSetBits(egEvents,SystemFireState);
+					if( xSemaphoreTake( xMutexLCD, ( TickType_t ) 400 ) == pdTRUE )
+					{
+						LCD_DisplayStringRowCol((uint8 *)"E",2,13);	
+						xSemaphoreGive( xMutexLCD );
+					}
+					else 
+					{
+
+					}
+				}
+
+			}
+		}
+		
+
+	}
+}
+
+
+void T_T6_SwitchBetHaz_Nor(void *pvparam)
+{
+	boolean Mutex_Taken_Already = FALSE ;
+	while (1)
+	{
+		ebValues = xEventGroupWaitBits(egEvents,
+										(SystemFireState | ExceedTemperature),
+										0,
+										1,//AND
+										100);
+
+		if( ( ebValues & (SystemFireState | ExceedTemperature) )== (SystemFireState | ExceedTemperature) )
+		{
+			if( xSemaphoreTake( xMutexLCD, ( TickType_t ) 1000 ) == pdTRUE )
+			{
+				Mutex_Taken_Already = TRUE ;
+				F_fireStateScreen();
+			}
+			// GPIO_TogglePin(PORTC_ID,PIN0_ID);
+			// vTaskDelay(300);
+		}
+		else
+		{
+			if(Mutex_Taken_Already == TRUE)
+			{
+				Mutex_Taken_Already = FALSE ;
+				xSemaphoreGive( xMutexLCD );
+				/*	Should be here to avoid continous update LCD without go from Fire state to normal state*/
+				LCD_MAINInit();	
+			}
+			// GPIO_TogglePin(PORTC_ID,PIN1_ID);
+			// vTaskDelay(300);
+		}
+		vTaskDelay(500);
+	}
+	
+}
+
+
+void System_Init(void)
+{
+	LCD_init();
+	USART_Init();
+    ADC_Init();
+    GPIO_init();
+
+}
+
+void LCD_MAINInit(void)
+{
+	LCD_ClearScreen();
+	LCD_DisplayStringRowCol((uint8 *)"Cur Temp:    C",0,0);	
+	/*	Catch value for Temperature and Display it  */
+	Current_Temp = NTC_GetTemperature();
+	LCD_MoveCursor(0,10);
+	LCD_intToString(Current_Temp);
+	LCD_DisplayStringRowCol((uint8 *)"Threshold Temp:    C",1,0);	
+	LCD_MoveCursor(1,16);
+	LCD_intToString(thresold_Temp);
+	LCD_DisplayStringRowCol((uint8 *)"Alarm State: ",2,0);	
+	LCD_MoveCursor(2,13);
+	if(AlarmState == TRUE)
+	{
+		LCD_DisplayCharacter('E');
+	}
+	else 
+	{
+		LCD_DisplayCharacter('D');
+	}
+	
+}
+
+
+void F_fireStateScreen(void)
+{
+	LCD_ClearScreen();
+	LCD_DisplayStringRowCol((uint8 *)"!! ALARM !!", 1 , 4);
+}
+
+
